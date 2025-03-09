7d8e4e9cae8a9b85d0485fd887f9e46e38b21eb5
Ali-Jasim <legolasplox@gmail.com>
2025-03-09 10:58:48 -0500

    api_update

--- backend/models/landlord.py
+++ backend/models/landlord.py
@@ -3,22 +3,23 @@ from sqlalchemy.orm import relationship, Session
 
 from models.base import Base
 
+
 class Landlord(Base):
     __tablename__ = "landlords"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     phone_number = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False, index=True)
-    
+
     # Use string references for relationships
     tenants = relationship("Tenant", back_populates="landlord")
     contractors = relationship("Contractor", back_populates="landlord")
     properties = relationship("Property", back_populates="landlord")
-    
+
     def __repr__(self):
         return f"<Landlord(name='{self.name}', email='{self.email}', phone_number='{self.phone_number}')>"
-    
+
     @staticmethod
     def create(db: Session, landlord_data: dict):
         landlord = Landlord(**landlord_data)
--- backend/models/property.py
+++ backend/models/property.py
@@ -3,20 +3,21 @@ from sqlalchemy.orm import relationship, Session
 
 from models.base import Base
 
+
 class Property(Base):
     __tablename__ = "properties"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     address = Column(String, nullable=False)  # Location information
     landlord_id = Column(Integer, ForeignKey("landlords.id"), nullable=False)
-    
+
     # Change relationship with tenant from one-to-many to one-to-one
     landlord = relationship("Landlord", back_populates="properties")
     tenant = relationship("Tenant", back_populates="property", uselist=False)
-    
+
     def __repr__(self):
         return f"<Property(address='{self.address}')>"
-    
+
     @staticmethod
     def create(db: Session, property_data: dict):
         property_obj = Property(**property_data)
